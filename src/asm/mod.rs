0b16265d03c4b2607ec063d090efd4e395cd9c76
gky360 <gky360@gmail.com>
2019-05-28 22:20:20 +0900

    add tests

--- src/asm/mod.rs
+++ src/asm/mod.rs
@@ -168,16 +168,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_display() {
-        use Ins::*;
+    fn test_display_main_only() {
         use Opr::*;
         use Reg::*;
 
         let fn_main = Function::new(
             "main",
             vec![
-                MOV(Direct(RAX), Literal(42)), // mov rax, 42
-                RET,                           // ret
+                Ins::MOV(Direct(RAX), Literal(42)), // mov rax, 42
+                Ins::RET,                           // ret
             ],
         );
         let assembly = Assembly(vec![
@@ -193,6 +192,55 @@ mod tests {
 
 main:
     mov rax, 42
+    ret
+
+"#;
+        assert_eq!(&format!("\n{}", assembly), expected);
+    }
+
+    #[test]
+    fn test_display_with_call() {
+        use Opr::*;
+        use Reg::*;
+
+        let fn_plus = Function::new(
+            "plus",
+            vec![
+                Ins::ADD(Direct(RSI), Direct(RDI)), // add rsi, rdi
+                Ins::MOV(Direct(RAX), Direct(RSI)), // add rsi, rdi
+                Ins::RET,                           // ret
+            ],
+        );
+        let fn_main = Function::new(
+            "main",
+            vec![
+                Ins::MOV(Direct(RDI), Literal(3)), // mov rdi, 3
+                Ins::MOV(Direct(RSI), Literal(4)), // mov rsi, 4
+                Ins::call("plus"),                 // call plus
+                Ins::RET,                          // ret
+            ],
+        );
+        let assembly = Assembly(vec![
+            Ent::dot("intel_syntax", "noprefix"),
+            Ent::dot("global", "plus, main"),
+            Ent::Empty,
+            Ent::Fun(fn_plus),
+            Ent::Fun(fn_main),
+        ]);
+
+        let expected = r#"
+.intel_syntax noprefix
+.global plus, main
+
+plus:
+    add rsi, rdi
+    mov rax, rsi
+    ret
+
+main:
+    mov rdi, 3
+    mov rsi, 4
+    call plus
     ret
 
 "#;
