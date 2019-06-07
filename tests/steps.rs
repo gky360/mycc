5fa927b386da51dd0b48ad4ef8bea05695881a01
gky360 <gky360@gmail.com>
2019-06-07 22:23:47 +0900

    fix tests

--- tests/steps.rs
+++ tests/steps.rs
@@ -1,7 +1,6 @@
 #[macro_use]
 extern crate log;
 
-use std::ffi::OsStr;
 use std::process::Command;
 use tempdir::TempDir;
 
@@ -170,7 +169,7 @@ fn step_14_call_func() {
 
         assert_output(
             "step_14/valid/call_func_01.c",
-            &[OsStr::new(&temp_file_name(&tmp_dir, "foo.o"))],
+            &[(&temp_file_name(&tmp_dir, "foo.o"))],
             &[],
             0,
             "OK\n",
@@ -178,7 +177,7 @@ fn step_14_call_func() {
         );
         assert_output(
             "step_14/valid/call_func_02.c",
-            &[OsStr::new(&temp_file_name(&tmp_dir, "foo_x_y.o"))],
+            &[(&temp_file_name(&tmp_dir, "foo_x_y.o"))],
             &[],
             0,
             "OK: 3, 12\n",
