071e1dfb4484b212659bbd287d4cf337d89f0b66
gky360 <gky360@gmail.com>
2019-06-15 02:53:59 +0900

    [step-18] fix tests

--- testdata/step_18/alloc4.c
+++ testdata/step_18/alloc4.c
@@ -1,7 +1,8 @@
 void alloc4(int **p, int n0, int n1, int n2, int n3) {
-    *p = malloc(4 * sizeof(int));
-    p[0] = n0;
-    p[1] = n1;
-    p[2] = n2;
-    p[3] = n3;
+    int *arr = malloc(4 * sizeof(int));
+    arr[0] = n0;
+    arr[1] = n1;
+    arr[2] = n2;
+    arr[3] = n3;
+    *p = arr;
 }
